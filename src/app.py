51c2f9cd14862507c7a02c982b492e681d564fb1
UdonDa <udoooon0727@gmail.com>
2018-06-09 15:24:46 +0900

    [Add]役職

--- src/app.py
+++ src/app.py
@@ -14,24 +14,26 @@ utils = Utils()
 def hello():
     return render_template('index.html')
 
+
 @app.route('/top')
 def route_top():
     return render_template('top.html')
+
 
 @app.route('/participants', methods=['GET', 'POST'])
 def get_participants():
     print("/participants")
     # TODO: /participantsへのpostの参加人数を受け取って、jsonに入れる
     num_p = 3
-    data[0]["participant"] = str(num_p)
-    print(data)
+    # data[0]["participant"] = str(num_p)
 
     # TODO: num_pの値から、プレイヤーの役職を決める
-    utils.decide_positions(num_p)
+    data = utils.decide_positions(num_p)
+    print(data)
     return render_template('play_position.html', num_p=num_p)
 
 
 if __name__ == '__main__':
 
-    app.run(host='0.0.0.0', port=80)
-    # app.run(host='0.0.0.0', port=5000)
+    # app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=5000)
--- src/src/utils.py
+++ src/src/utils.py
@@ -16,9 +16,15 @@ class Utils():
         self.positions = ['新規', 'ジロリアン', '一般人', '店員', 'かえぽん']
 
     def decide_positions(self, num_p):
+        """
+        return: self.data
+        """
+        self.data[0]["participant"] = num_p
         random.shuffle(self.positions)
         for i in range(num_p):
-            self.data[0]["player_{}".format(i+1)] = self.positions[i]
+            self.data[0]["player_{}".format(
+                i+1)]['position'] = self.positions[i]
         if '新規' not in self.positions[:num_p]:
-            self.data[0]['player_2']['positions'] = "新規"
-        print(self.data)
+            # TODO : player_2　の2をランダムに
+            self.data[0]['player_2']['position'] = "新規"
+        return self.data

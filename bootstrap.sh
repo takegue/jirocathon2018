d66be5b75452726f1c1b2050fad0c8b99ce12785
Shunsuke TAKENO <shunsuke.takeno@retty.me>
2018-06-09 12:04:19 +0900

    動くようになった

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 docker-compose build
-docker-compose up
+docker-compose up -d
+
+url=$(docker-compose port ngrok 4040)
+public=''
+while [[ $public -eq '' ]];do
+  public=$(curl -sq $url/status | grep -o '\w\+.ap.ngrok.io' | uniq)
+done
 

4109ed0e632533b83bf1001ebe4f7aa8724875af
UdonDa <udoooon0727@gmail.com>
2018-06-09 15:12:54 +0900

    [Fix]port

--- src/app.py
+++ src/app.py
@@ -30,5 +30,5 @@ def get_participants():
 
 if __name__ == '__main__':
 
-    # app.run(host='0.0.0.0', port=80)
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=80)
+    # app.run(host='0.0.0.0', port=5000)

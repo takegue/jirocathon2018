1c4de84b81c7fc174d0b02dba16110433903fd3a
UdonDa <udoooon0727@gmail.com>
2018-06-09 14:08:00 +0900

    [Add]参加人数受け取り

--- src/app.py
+++ src/app.py
@@ -1,11 +1,27 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 app = Flask(__name__)
+import json
+data = open('../data/data.json', 'r')
+data = json.load(data)
 
 
 @app.route('/')
 def hello():
+    print(data)
+    return render_template('index.html')
+
+
+@app.route('/participants', methods=['GET', 'POST'])
+def get_participants():
+    print("/participants")
+    # TODO: /participantsへのpostの参加人数を受け取って、jsonに入れる
+    p = 3
+    data[0]["participant"] = str(p)
+    print(data)
     return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+
+    # app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=5000)
